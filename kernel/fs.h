ef7c86f2a3fe1baa383f053fc787030c63aa7db3
Kevin <kevin@example.com>
2023-02-05 20:26:23 -0800

    found issue due to unchanged MAXFILE, but now bigfile test hangs

--- kernel/fs.h
+++ kernel/fs.h
@@ -27,7 +27,7 @@ struct superblock {
 #define NDIRECT 11
 #define NINDIRECT (BSIZE / sizeof(uint)) // should be 256
 #define NNINDIRECT (NINDIRECT * NINDIRECT)
-#define MAXFILE (NDIRECT + NINDIRECT)
+#define MAXFILE (NDIRECT + NINDIRECT + NNINDIRECT)
 
 // On-disk inode structure
 struct dinode {
