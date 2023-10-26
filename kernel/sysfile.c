568cb3097cb4233fd5477e8defc71ebd452da5c2
Kevin <kevin@example.com>
2023-10-25 21:12:12 -0700

    add clarifying docs to mmap and munmap

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -485,6 +485,22 @@ sys_pipe(void)
   return 0;
 }
 
+// mmap can be called in many ways, but this lab requires only a subset of its
+// features relevant to memory-mapping a file. You can assume that addr will always
+// be zero, meaning that the kernel should decide the virtual address at which to
+// map the file. mmap returns that address, or 0xffffffffffffffff if it fails.
+// length is the number of bytes to map; it might not be the same as the file's
+// length. prot indicates whether the memory should be mapped readable, writeable,
+// and/or executable; you can assume that prot is PROT_READ or PROT_WRITE or both.
+// flags will be either MAP_SHARED, meaning that modifications to the mapped memory
+// should be written back to the file, or MAP_PRIVATE, meaning that they should
+// not. You don't have to implement any other bits in flags. fd is the open file
+// descriptor of the file to map. You can assume offset is zero (it's the starting
+// point in the file at which to map).
+//
+// It's OK if processes that map the same MAP_SHARED file do not share physical
+pages.
+
 uint64
 sys_mmap(void)
 {
@@ -530,6 +546,12 @@ sys_mmap(void)
   return v->addr;
 }
 
+// munmap(addr, length) should remove mmap mappings in the indicated address range.
+// If the process has modified the memory and has it mapped MAP_SHARED, the
+// modifications should first be written to the file. An munmap call might cover
+// only a portion of an mmap-ed region, but you can assume that it will either
+// unmap at the start, or at the end, or the whole region (but not punch a hole in
+// the middle of a region).
 uint64
 sys_munmap(void)
 {
